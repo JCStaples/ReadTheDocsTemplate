29ed92d0c0dea9e733493fe953a954adaf029359
JCStaples <59157666+JCStaples@users.noreply.github.com>
2023-01-15 13:08:33 +1100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Kobu Gaming'
+copyright = '2021, Kobu'
+author = 'Sholtor'
 
 release = '0.1'
 version = '0.1.0'
